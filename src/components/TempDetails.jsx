c14183f960cc8969187d9bde7b0093a5bd38c348
Janu <abbyjunior600@gmail.com>
2023-02-02 10:29:33 +0700

    integrated with weather API and add some function

--- src/components/TempDetails.jsx
+++ src/components/TempDetails.jsx
@@ -5,35 +5,52 @@ import {
   UilWind,
   UilSun,
   UilSunset,
+  UilArrowUp,
+  UilArrowDown,
 } from "@iconscout/react-unicons";
+import { formatLocalTime, iconUrl } from "../utils/services/service";
 
-function TempDetails() {
+function TempDetails({
+  units,
+  weather: {
+    details,
+    icon,
+    temp,
+    temp_min,
+    temp_max,
+    sunrise,
+    sunset,
+    speed,
+    humidity,
+    feels_like,
+    timezone,
+  },
+}) {
   return (
     <div className="flex flex-col items-center gap-5">
-      <h2 className="text-white text-lg">Cloudy</h2>
+      <h2 className="text-white text-lg">{details}</h2>
       <div className="w-full flex flex-row justify-between items-center gap-10">
         <div className="w-1/3 flex items-center justify-center">
-          <img
-            className="w-20"
-            src="http://openweathermap.org/img/wn/04n.png"
-            alt=""
-          />
+          <img className="w-20" src={iconUrl(icon)} alt={details} />
         </div>
         <h1 className="w-1/3 text-4xl text-center text-white font-light">
-          19&deg;C
+          {temp.toFixed()}&deg;{units === "metric" ? "C" : "F"}
         </h1>
         <div className="w-1/3 flex flex-col items-start gap-1 text-sm text-white font-light">
           <div className="flex items-center gap-0.5">
             <UilTemperature size={18} />
-            Real Felt: <span className="font-medium">18&deg;C</span>
+            Real Feel:{" "}
+            <span className="font-medium">
+              {feels_like.toFixed()}&deg;{units === "metric" ? "C" : "F"}
+            </span>
           </div>
           <div className="flex items-center gap-0.5">
             <UilTear size={18} />
-            Humidity: <span className="font-medium">50%</span>
+            Humidity: <span className="font-medium">{humidity.toFixed()}%</span>
           </div>
           <div className="flex items-center gap-0.5">
             <UilWind size={18} />
-            Wind: <span className="font-medium">4 km/h</span>
+            Wind: <span className="font-medium">{speed.toFixed()} km/h</span>
           </div>
         </div>
       </div>
@@ -42,7 +59,7 @@ function TempDetails() {
           <UilSun size={18} />
           Rise:
           <span span className="font-medium">
-            04:50 AM
+            {formatLocalTime(sunrise, timezone, "hh:mm a")}
           </span>
         </div>
         <span className="h-4 w-[1px] bg-gray-200 rounded-sm"></span>
@@ -50,23 +67,23 @@ function TempDetails() {
           <UilSunset size={18} />
           Set:
           <span span className="font-medium">
-            04:50 AM
+            {formatLocalTime(sunset, timezone, "hh:mm a")}
           </span>
         </div>
         <span className="h-4 w-[1px] bg-gray-200 rounded-sm"></span>
         <div className="flex items-center gap-1">
-          <UilSunset size={18} />
+          <UilArrowUp size={18} />
           High:
           <span span className="font-medium">
-            21&deg;C
+            {temp_max.toFixed()}&deg;{units === "metric" ? "C" : "F"}
           </span>
         </div>
         <span className="h-4 w-[1px] bg-gray-200 rounded-sm"></span>
         <div className="flex items-center gap-1">
-          <UilSunset size={18} />
+          <UilArrowDown size={18} />
           Low:
           <span span className="font-medium">
-            21&deg;C
+            {temp_min.toFixed()}&deg;{units === "metric" ? "C" : "F"}
           </span>
         </div>
       </div>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Forecast from "../components/Forecast";
 import Header from "../components/Header";
 import SearchForm from "../components/SearchForm";
 import TempDetails from "../components/TempDetails";
 import TimeAndLocation from "../components/TimeAndLocation";
+import getFormatedWeatherData from "../utils/services/service";
 
 function App() {
+  const [query, setQuery] = useState({ q: "surabaya" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormatedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
   return (
     <div className="min-h-screen max-w-screen-xl py-5">
       <div className="mx-auto w-11/12 sm:w-10/12 lg:w-8/12 h-full bg-gradient-to-br from-cyan-300 to-blue-600 rounded-md shadow-md flex flex-col items-center gap-5 p-5">
-        <Header />
-        <SearchForm />
-        <TimeAndLocation />
-        <TempDetails />
-        <Forecast />
+        <Header setQuery={setQuery} />
+        <SearchForm setQuery={setQuery} units={units} setUnits={setUnits} />
+        {weather && (
+          <div className="w-full flex flex-col items-center gap-5">
+            <TimeAndLocation weather={weather} />
+            <TempDetails units={units} weather={weather} />
+            <Forecast
+              units={units}
+              hourly={weather.hourly}
+              weekly={weather.weekly}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
